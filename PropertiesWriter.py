3bd45d12afb8cae40453dc9deeea2079076561e9
Brian Busemeyer <bbusemeyer@gmail.com>
2016-12-05 17:25:58 -0600

    Made PropertiesWriter consistent with CrystalWritr

--- PropertiesWriter.py
+++ PropertiesWriter.py
@@ -1,17 +1,18 @@
 
 
 class PropertiesWriter:
-  def __init__(self):
-    self.kmesh=[2,2,2]
-    self.cryapi=False
-    self.gmesh=16
-
+  # Should we require a CrystalWriter object?
+  def __init__(self,crystal_writer,cryapi=False):
+    self.kmesh=crystal_writer.kmesh
+    self.cryapi=cryapi
+    self.gmesh=crystal_writer.kmesh
+    self.boundary=crystal_writer.boundary
 
   def properties_input(self):
     outlines=['NEWK']
-    if self.cif!=None:
+    if self.boundary=='3d':
       outlines+=[ "0 %i"%self.gmesh,
-                  " ".join(map(str,self.kmesh)),
+                  " ".join(map(str,self.kmesh))]
     outlines+=["1 1"]
     if self.cryapi:
       outlines+=["CRYAPI_OUT"]
