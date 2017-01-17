207edc90bab9485871c93c5c3aab99796b926051
Brian Busemeyer <bbusemeyer@gmail.com>
2017-01-17 11:46:08 -0600

    Forgot to include new make_plan files.

---
+++ example/make_crystal_plan.py
@@ -0,0 +1,65 @@
+import sys
+sys.path.append("../")
+
+from JobEnsemble import JobEnsemble
+import Recipes as recipes
+import pickle as pkl
+from copy import deepcopy
+import os
+
+dft_opts={
+    'xml_name':os.getcwd()+'/../BFD_Library.xml',
+    'basis_params':[0.2,0,3],
+    'cutoff':0.0,
+    'dftgrid':'LGRID',
+    'spin_polarized':False
+  }
+
+variance_opts={
+    'iterations':15
+    }
+
+energy_opts={
+    'total_nstep':512
+    }
+
+dmc_opts={
+    'timesteps':[.03]
+    }
+
+pbc_opts={
+    'xml_name':os.getcwd()+'/../BFD_Library.xml',
+    'basis_params':[0.4,1,3],
+    'cutoff':0.2,
+    'dftgrid':'LGRID',
+    'spin_polarized':False,
+    'kmesh':[2,2,2] 
+  }
+
+test = JobEnsemble([
+
+    recipes.LocalCrystalQWalk('h2',open('h2.xyz','r').read(),
+      crystal_opts=dft_opts,
+      variance_opts=variance_opts,
+      energy_opts=energy_opts,
+      dmc_opts=dmc_opts,
+      structtype='xyz'),
+
+    recipes.LocalCrystalQWalk('n2',open('n2.xyz','r').read(),
+      crystal_opts=dft_opts,
+      variance_opts=variance_opts,  
+      energy_opts=energy_opts,
+      dmc_opts=dmc_opts,      
+      structtype='xyz'),
+
+    recipes.LocalCrystalQWalk('si',open('si.cif','r').read(),
+      crystal_opts=pbc_opts,
+      variance_opts=variance_opts,
+      energy_opts=energy_opts,
+      dmc_opts=dmc_opts,            
+      structtype='cif')
+    
+  ])
+
+with open('plan.pickle','wb') as outf:
+  pkl.dump(test,outf)
---
+++ example/make_pyscf_plan.py
@@ -0,0 +1,50 @@
+import sys
+sys.path.append("../")
+
+from JobEnsemble import JobEnsemble
+import Recipes as job
+import pickle as pkl
+from copy import deepcopy
+import os
+from QWalkRunner import LocalQWalkRunner
+
+dft_opts={
+    "xyz":"H 0. 0. 0.; H 0. 0. 0.7",
+    "pyscf_path":["/home/brian/programs/","../../"]
+  }
+
+dft_opts_n2={'xyz':"N 0. 0. 0.; N 0. 0. 2.5",
+    "pyscf_path":["/home/brian/programs/","../../"]
+  }
+    
+
+variance_opts={
+    'iterations':15
+    }
+
+energy_opts={
+    'total_nstep':512
+    }
+
+dmc_opts={
+    'timesteps':[.03]
+    }
+
+test = JobEnsemble([
+#    job.PySCFQWalk('h2',"H 0. 0. 0.; H 0. 0. 0.7",
+#      pyscf_opts=dft_opts,
+#      variance_opts=variance_opts,
+#      energy_opts=energy_opts,
+#      dmc_opts=dmc_opts,
+#      qwalkrunner=LocalQWalkRunner() ),
+  job.PySCFQWalk('n2',"H 0. 0. 0.; H 0. 0. 0.7",
+     pyscf_opts=dft_opts_n2,
+     variance_opts=variance_opts,
+     energy_opts=energy_opts,
+     dmc_opts=dmc_opts,
+     qwalkrunner=LocalQWalkRunner() )
+  ]
+  )
+
+with open('plan.pickle','wb') as outf:
+  pkl.dump(test,outf)
