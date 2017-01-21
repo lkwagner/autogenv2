1813d31b221d04e31ec0f191d9ea3093a26ef51b
Lucas K. Wagner <lucas.wagner@gmail.com>
2017-01-21 12:36:30 -0600

    Undid the changes in generate_report(). The issue is that the manager doesn't know that it should average over k-points. That's a decision for the Recipe.

--- example/make_pyscf_plan.py
+++ example/make_pyscf_plan.py
@@ -8,11 +8,6 @@ from copy import deepcopy
 import os
 from QWalkRunner import QWalkRunnerPBS
 
-# TODO
-#dft_opts={
-#    "xyz":"H 0. 0. 0.; H 0. 0. 0.7"
-#  }
-
 hf_opts={
     'xyz':"N 0. 0. 0.; N 0. 0. 2.5",
     'method':'ROHF',
@@ -41,13 +36,14 @@ energy_opts={
 
 dmc_opts={
     'timesteps':[.03],
-    'extra_observables':[{
-      'name':'average_derivative_dm',
-      'nmo':8,
-      'orbfile':'qw.orb',
-      'basis':'qw.basis',
-      'states':[3,4,5,6,7,8]
-    }]
+#    'extra_observables':[{
+#      'name':'average_derivative_dm',
+#      'nmo':8,
+#      'orbfile':'qw.orb',
+#      'basis':'qw.basis',
+#      'states':[3,4,5,6,7,8]
+#    }]
+     'extra_observables':[ {'name':'region_fluctuation','maxn':6}] 
   }
 
 test = JobEnsemble([
