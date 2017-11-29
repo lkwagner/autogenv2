159090afe7073fc23843416d09da215bd3916a04
Brian Busemeyer <bbusemeyer@gmail.com>
2017-11-29 15:03:57 -0600

    crystal2pyscf now works for periodic systems.

--- crystal2pyscf.py
+++ crystal2pyscf.py
@@ -7,7 +7,7 @@ from crystal2qmc import periodic_table,read_gred, read_kred, read_outputfile
 import pyscf
 import pyscf.pbc
 import pyscf.pbc.dft
-
+from collections import Counter
 
 # PySCF basis hardcoded for now.
 basis={
@@ -83,29 +83,28 @@ def crystal2pyscf_mol(propoutfn="prop.in.o",basis='bfd_vtz'):
 ##########################################################################################################
 def crystal2pyscf_cell(propoutfn="prop.in.o",
     basis='bfd_vtz',
-    gs=(8,8,8)):
+    gs=(8,8,8),
+    basis_order=None):
   ''' Make a PySCF object with solution from a crystal run.
 
   Args:
     propoutfn (str): properties or crystal stdout.
     basis (str): PySCF basis option--should match the crystal basis.
   Returns:
-    tuple: (mol,scf) PySCF-equilivent Mole and SCF object.
+    tuple: (cell,scf) PySCF-equilivent Mole and SCF object.
   '''
   #TODO Make basis from crystal output (fix normalization issue).
   #TODO Generalize spin and kpoint.
 
-  # DEBUG available reference.
-  ref_chkfile="../pyscf/pyscf_driver.py.chkfile"
-  check_cell=pyscf.pbc.lib.chkfile.load_cell(ref_chkfile)
-  check_mf=pyscf.pbc.dft.KRKS(check_cell)
-  check_mf.__dict__.update(pyscf.lib.chkfile.load(ref_chkfile,'scf'))
-
+  ## DEBUG available reference.
+  #ref_chkfile="../pyscf/pyscf_driver.py.chkfile"
+  #check_cell=pyscf.pbc.lib.chkfile.load_cell(ref_chkfile)
+  #check_mf=pyscf.pbc.dft.KRKS(check_cell)
+  #check_mf.__dict__.update(pyscf.lib.chkfile.load(ref_chkfile,'scf'))
 
   # Load crystal data.
   info, crylat_parm, cryions, crybasis, crypseudo = read_gred()
   cryeigsys = read_kred(info,crybasis)
-
 
   # Format and input structure.
   atom=[
@@ -133,16 +132,49 @@ def crystal2pyscf_cell(propoutfn="prop.in.o",
 
   # Copy over MO info.
   # TODO only Gamma for now.
-  mf=check_mf
-  print(check_mf.mo_energy)
-  crydf=format_eigenstates_cell(cell,cryeigsys)
+  #mf=check_mf
+  crydf=format_eigenstates_cell(cell,cryeigsys,basis_order)
   mf.mo_energy=cryeigsys['eigvals']
   mf.mo_coeff=[crydf[[*range(crydf.shape[0])]].values]
-  #mf.mo_occ=[cryeigsys['eig_weights'][0][0]*2]
-  print(mf.mo_energy)
+  mf.mo_occ=[(cryeigsys['eig_weights'][0][0]>1e-8).astype(float)*2]
   mf.e_tot=np.nan #TODO compute energy and put it here, if needed.
 
   return cell,mf
+
+##########################################################################################################
+def fix_basis_order(basis_order):
+  ''' Return the order to rearrange the basis to order it by angular momentum (like PySCF).
+
+  Example:
+    >>> # for 's','p','d','s','s','p','p','d','d','f'
+    >>> fix=_fix_basis_order([0,1,2,0,0,1,1,2,2,3])
+    >>> print(fix)
+
+    array([0,9,10,1,2,3,11,12,13,
+           14,15,16,4,5,6,7,8,17,
+           18,19,20,21,22,23,24,
+           25,26,27,28,29,30,31,32,33])
+  Args:
+    basis_order (list): Current basis order.
+  Returns:
+    ndarray: index for reordering.
+  '''
+  # Sort by l
+  ams=[]
+  for i,l in enumerate(basis_order):
+    ams+=[l for i in range(2*l+1)]
+  amsorted=np.argsort(ams)
+  
+  # Sort within l by order of appearance.
+  start=0
+  counts=dict(Counter(basis_order))
+  for l in range(0,max(basis_order)+1):
+    counts[l]=(2*l+1)*counts[l]
+    end=start+counts[l]
+    amsorted[start:end]=sorted(amsorted[start:end])
+    start+=counts[l]
+
+  return amsorted
 
 ##########################################################################################################
 def format_eigenstates_mol(mol,cryeigsys):
@@ -174,28 +206,45 @@ def format_eigenstates_mol(mol,cryeigsys):
   return crydf
 
 ##########################################################################################################
-def format_eigenstates_cell(cell,cryeigsys):
+def format_eigenstates_cell(cell,cryeigsys,basis_order=None):
   ''' Organize crystal eigenstates to be consistent with PySCF order.
 
   Args: 
     cell (Cell): Contains structure in PySCF object.
     cryeigsys (dict): eigenstate info from cryeigsys.
+    basis_order (list): order that basis set is entered. 
+      None means it's sorted by angular momentum, like PySCF.
+      Example: for 's','p','d','s','s','p','p','d','d',
+        use [0,1,2,0,0,1,1,2,2,3].
   Returns:
     DataFrame: eigenstates in correct order, with labeling.
   '''
-  # DEBUG available reference.
-  ref_chkfile="../pyscf/pyscf_driver.py.chkfile"
-  check_cell=pyscf.pbc.lib.chkfile.load_cell(ref_chkfile)
-  check_mf=pyscf.pbc.dft.KRKS(check_cell)
-  check_mf.__dict__.update(pyscf.pbc.lib.chkfile.load(ref_chkfile,'scf'))
+  ## DEBUG available reference.
+  #ref_chkfile="../pyscf/pyscf_driver.py.chkfile"
+  #check_cell=pyscf.pbc.lib.chkfile.load_cell(ref_chkfile)
+  #check_mf=pyscf.pbc.dft.KRKS(check_cell)
+  #check_mf.__dict__.update(pyscf.pbc.lib.chkfile.load(ref_chkfile,'scf'))
 
   # Extract.
   # TODO non-Gamma points.
   crydf=pd.DataFrame(np.array(cryeigsys['eigvecs'][(0,0,0)]['real'][0]).T)
 
-  # Label by patching PySCF order.
+  # PySCF basis order (our goal).
   pydf=pd.DataFrame(cell.spherical_labels(),columns=['atnum','elem','orb','type'])
-  crydf=crydf.join(pydf[['atnum','elem','orb','type']])
+
+  # Info about atoms.
+  crydf=crydf.join(pydf[['atnum','elem']])
+
+  # Reorder basis.
+  def _apply_fix(df):
+    elem=df['elem'].values[0]
+    fixed_basis_order=fix_basis_order(basis_order[elem])
+    return df.reset_index(drop=True).loc[fixed_basis_order]
+  if basis_order is not None:
+    crydf=crydf.groupby(['atnum','elem']).apply(_apply_fix).reset_index(drop=True)
+
+  # Label by patching PySCF order.
+  crydf=crydf.join(pydf[['orb','type']])
   crystal_order=('', 'x', 'y', 'z', 'z^2', 'xz', 'yz',  'x2-y2', 'xy',   'z^3', 'xz^2', 'yz^2', 'zx^2', 'xyz',  'x^3',  'y^3')
   pyscf_order=  ('', 'x', 'y', 'z', 'xy',  'yz', 'z^2', 'xz',   'x2-y2', 'y^3', 'xyz',  'yz^2', 'z^3',  'xz^2', 'zx^2', 'x^3')
   orbmap=dict(zip(pyscf_order,crystal_order))
@@ -207,12 +256,12 @@ def format_eigenstates_cell(cell,cryeigsys):
   # Reorder crydf.
   crydf=pydf.merge(crydf,on=['atnum','elem','orb','type'])
 
-  # DEBUG check a vector
-  vector=29
-  #print(pd.DataFrame({'energy':check_mf.mo_energy[0]}))
-  crydf['check']=check_mf.mo_coeff[0][:,vector].real
-  crydf['diff']=crydf[vector]-crydf['check']
-  #print(crydf[['atnum','elem','orb','type','check',vector,'diff']].round(4))
+  ## DEBUG check a vector
+  #vector=0
+  ##print(pd.DataFrame({'energy':check_mf.mo_energy[0]}))
+  #crydf['check']=check_mf.mo_coeff[0][:,vector].real
+  #crydf['diff']=crydf[vector]-crydf['check']
+  ##print(crydf[['atnum','elem','orb','type','check',vector,'diff']].round(4))
   return crydf
 
 ##########################################################################################################
@@ -336,7 +385,7 @@ def test_crystal2pyscf_mol(ref_chkfile="pyscf_driver.py.o",propoutfn="prop.in.o"
   print("\n\n === All tests passed! Good on you, friend! ===")
 
 ##########################################################################################################
-def test_crystal2pyscf_cell(ref_chkfile="pyscf_driver.py.o",propoutfn="prop.in.o"):
+def test_crystal2pyscf_cell(ref_chkfile="pyscf_driver.py.o",propoutfn="prop.in.o",basis_order=None):
   ''' Test converter by reading an identical PySCF run and checking results are consistent.'''
   #TODO Doc
 
@@ -346,7 +395,7 @@ def test_crystal2pyscf_cell(ref_chkfile="pyscf_driver.py.o",propoutfn="prop.in.o
   check_mf.__dict__.update(pyscf.lib.chkfile.load(ref_chkfile,'scf'))
 
   # Result of conversion.
-  cell,mf=crystal2pyscf_cell(basis=basis,gs=(8,8,8))
+  cell,mf=crystal2pyscf_cell(basis=basis,gs=(8,8,8),basis_order=basis_order)
 
   #print("DIFF")
   #for key in cell.__dict__.keys():
@@ -374,12 +423,14 @@ def test_crystal2pyscf_cell(ref_chkfile="pyscf_driver.py.o",propoutfn="prop.in.o
   # Energies:
   print("\nBeginning tests...\n")
   print("  Energy test:")
-  print("    Reference energy...")
-  check_energy=compute_energy_cell(check_cell,check_mf)
   print("    Test energy...")
   test_energy=compute_energy_cell(cell,mf)
+  print("    {} Ha".format(test_energy))
+  print("    Reference energy...")
+  check_energy=compute_energy_cell(check_cell,check_mf)
+  print("    {} Ha".format(check_energy))
   err=test_energy-check_energy
-  etest=abs(err)<1e-8
+  etest=abs(err)<1e-5
   if not etest: 
     print("    Energy test failed; test: {:.8} Ha, check {:.8} Ha.".format(test_energy,check_energy))
   else: 
@@ -391,5 +442,17 @@ def test_crystal2pyscf_cell(ref_chkfile="pyscf_driver.py.o",propoutfn="prop.in.o
     print("\n\n === All tests passed! Good on you, friend! ===")
 
 ##########################################################################################################
+def test_counter():
+  print("These should match:")
+  print(np.array([0,9,10,1,2,3,11,12,13,
+           14,15,16,4,5,6,7,8,17,
+           18,19,20,21,22,23,24,
+           25,26,27,28,29,30,31,32]))
+  print(fix_basis_order([0,1,2,0,0,1,1,2,2,3]))
+
+##########################################################################################################
 if __name__=='__main__':
-  test_crystal2pyscf_cell(ref_chkfile="../pyscf/pyscf_driver.py.chkfile")
+  pass
+  # DEBUG
+  #test_crystal2pyscf_cell(ref_chkfile="../pyscf/pyscf_driver.py.chkfile",basis_order={'Si':[0,1,2,3,0,0,1,1]})
+  #_test_counter()
