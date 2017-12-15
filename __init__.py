23fde0105c31311c8d7bb09199241ef37cb13b5a
Brian Busemeyer <bbusemeyer@gmail.com>
2017-12-14 18:24:05 -0600

    Fixed file names for this repo.

--- __init__.py
+++ __init__.py
@@ -1,12 +1,42 @@
+import average_tools
+import crystal2qmc
+import Crystal2QMCReader
+import Crystal2QMCRunner
+import Crystal
+import CrystalRunner
+import DMC
+import JobEnsemble
+import Linear import Manager
+import Postprocess
+import PropertiesReader
+import PropertiesRunner
+import pyscf2qwalk
 import PySCF
 import PySCFRunner
-import Crystal
-import PySCFRunner
-import PropertiesReader
+import QWalkRunner
+import Recipes
+import submitter
 import Variance
-import Linear
-import DMC
-import Runner
-import Manager
 
-__all__=['PySCF,PySCFRunner','Crystal','PySCFRunner','Variance','Linear','DMC','Runner','Manager']
+__all__=[
+    'average_tools.py',
+    'crystal2qmc.py',
+    'Crystal2QMCReader.py',
+    'Crystal2QMCRunner.py',
+    'Crystal.py',
+    'CrystalRunner.py',
+    'DMC.py',
+    'JobEnsemble.py',
+    'Linear.py',
+    'Manager.py',
+    'Postprocess.py',
+    'PropertiesReader.py',
+    'PropertiesRunner.py',
+    'pyscf2qwalk.py',
+    'PySCF.py',
+    'PySCFRunner.py',
+    'QWalkRunner.py',
+    'Recipes.py',
+    'submitter.py',
+    'Variance.py'
+  ]
