2540afd17affea89199c38848ca14850e5db27ea
Brian Busemeyer <bbusemeyer@gmail.com>
2017-12-19 15:53:29 -0600

    Bug with properties

--- Manager.py
+++ Manager.py
@@ -125,7 +125,7 @@ class CrystalManager:
     if status=="not_started":
       sh.copy(self.crysinpfn,'INPUT')
       self.runner.run("Pcrystal &> %s"%self.crysoutfn)
-      self.runner.postfix += ["cp %s INPUT"%self.propoutfn,"Pproperties &> %s"%self.propinpfn]
+      self.runner.postfix += ["cp %s INPUT"%self.propoutfn,"Pproperties &> %s.o"%self.propinpfn]
     elif status=="ready_for_analysis":
       #This is where we (eventually) do error correction and resubmits
       status=self.creader.collect(self.crysoutfn)
